2dfd3d1188dac7b50e91ae351ddd8c05a7cf94aa
localghost <localghost@codeleet.com>
2014-03-23 09:55:06 +0100

    Add compatibility macros
    These macros are for the sake of compilers which do not yet implement all the
    new features from C++11 (e.g. final, override, etc.).

---
+++ base/compatibility.h
@@ -0,0 +1,14 @@
+#ifndef BASE_COMPABILITY_H
+#define BASE_COMPABILITY_H
+
+#include "inspect.h"
+
+#if SUPPORTED(CPP11)
+#define FINAL final
+#define OVERRIDE override
+#else
+#define FINAL
+#define OVERRIDE
+#endif
+
+#endif
