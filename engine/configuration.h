86f751ecbfb1d47a578bbe2fa0af0f54b52620ca
localghost <localghost@codeleet.com>
2014-03-23 09:50:36 +0100

    Update configuration

--- engine/configuration.h
+++ engine/configuration.h
@@ -5,45 +5,50 @@
 #include <functional>
 #include <utility>
 #include <boost/filesystem/path.hpp>
+#include <boost/signals2.hpp>
 
 template<typename T>
-class configuration_element
+FINAL class configuration_element
 {
+    typedef boost::signals2::signal<void(const T&)> signal_type;
+
 public:
     typedef T value_type;
+    typedef signal_type::slot_type slot_type;
 
     configuration_element() = default;
 
-    configuration_element(T value) : value(std::move(value)) { }
+    configuration_element(T value) : value_(std::move(value)) { }
 
-    T value() const { return value; }
-
+    T value() const { return value_; }
     void set_value(T value)
     {
-        this->value = std::move(value);
-        for (const auto& observer : observers)
-            observer(this->value);
+        value_ = std::move(value);
+        on_value_change_(value_);
     }
 
-    template<typename ObserverType>
-    void add_observer(ObserverType observer)
+    boost::signals2::scoped_connection connect(const slot_type& slot)
     {
-        observers.push_back(std::move(observer));
+        return on_value_change_.connect(slot);
     }
 
 private:
-    T value;
-    std::vector<std::function<void(configuration_element)>> observers;
+    T value_;
+    signal_type on_value_change_;
 };
 
-struct configuration
+FINAL struct configuration
 {
-public:
-    static configuration from_file(const boost::filesystem::path& path);
+    static configuration load(const boost::filesystem::path& path);
+    static void save(const boost::filesystem::path& path);
 
-    boost::filesystem::path cache;
-    std::string username;
-    encrypted_string password;
+    configuration_element<boost::filesystem::path> cache;
+    configuration_element<std::string> username;
+    // FIXME use when encrypted<string> is implemented
+    // configuration_element<encrypted<string>> password;
+    configuration_element<std::string> password;
 };
+
+class configurator
 
 #endif
