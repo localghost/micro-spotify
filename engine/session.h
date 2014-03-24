fec56bfd4b0869841f8663e3e6b8e9f9df03b8fe
Zbigniew Kostrzewa <z.kostrzewa@samsung.com>
2014-03-24 08:08:06 +0100

    Refactor session

--- engine/session.h
+++ engine/session.h
@@ -4,8 +4,18 @@
 #include <libspotify/api.h>
 
 namespace engine {
-class session
+FINAL class session
 {
+    typedef boost::signals2::signal<void(sp_error)> logged_in_signal_type;
+
+public:
+    typedef logged_in_signal_type::slot_type logged_in_slot_type;
+
+    session();
+    ~session();
+
+    boost::signals2::scoped_connection connect_logged_in(const logged_in_slot_type& slot);
+
 private:
     void setup_configuration();
 
