60a1c2cb4050fc650fe51ec2c35af46972686d22
localghost <localghost@codeleet.com>
2014-03-26 21:27:14 +0100

    Add tests for configuration element

---
+++ tests/base/test_configuration_element.cc
@@ -0,0 +1,39 @@
+#include <boost/test/unit_test.hpp>
+
+#include <base/configuration_element.h>
+
+BOOST_AUTO_TEST_SUITE(TSBaseConfigurationElement)
+
+BOOST_AUTO_TEST_CASE(TCBaseConfigurationElementNotified)
+{
+    bool pass = false;
+
+    base::configuration_element<std::string> ces;
+    ces.connect([&](const std::string&)
+    {
+        pass = true;
+    });
+    ces.set_value("new value");
+
+    BOOST_CHECK(pass);
+}
+
+BOOST_AUTO_TEST_CASE(TCBaseConfigurationElementNotifiedNewValue)
+{
+    bool pass = false;
+
+    base::configuration_element<std::string> ces{"init value"};
+    ces.connect([&](const std::string& new_value)
+    {
+        if ("new value" == new_value)
+            pass = true;
+    });
+
+    BOOST_CHECK_EQUAL(ces.value(), "init value");
+
+    ces.set_value("new value");
+
+    BOOST_CHECK(pass);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
