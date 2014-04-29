359604ea43d486deae227f5a5330fb84238c599e
localghost <localghost@codeleet.com>
2014-04-29 14:38:48 +0200

    Init implementation with priority_queue

--- base/message_loop.cc
+++ base/message_loop.cc
@@ -21,13 +21,15 @@ void message_loop::stop()
   queue_task(task{[this]() { active_ = false; }});
 }
 
-void message_loop::queue_task(task task_)
+void message_loop::queue_task(task task_, std::chrono::milliseconds delay)
 {
-  queue_.push(std::move(task_));
+  // FIXME protect with mutex
+  queue_.emplace(traits::high_steady_clock::time_point{delay}, std::move(task_));
 }
 
 void message_loop::exec()
 {
+  // FIXME protect with mutex
   while (active_)
   {
     task t = queue_.pop();
--- base/message_loop.h
+++ base/message_loop.h
@@ -1,33 +1,42 @@
 #ifndef BASE_MESSAGE_LOOP_H
 #define BASE_MESSAGE_LOOP_H
 
+#include <queue>
 #include <condition_variable>
 #include <base/task.h>
-#include <base/thread_safe_queue.h>
+#include <base/traits.h>
 
 namespace base {
 // Tasks will be executed on the thread the start() method was invoked on.
 class message_loop FINAL
 {
+private:
   struct queued_task
   {
-    high_steady_clock::time_point when_;
+    traits::high_steady_clock::time_point when_;
     task task_;
+
+    friend bool operator<(const queued_task& x, const queued_task& y) const
+    {
+      return x.when_ < y.when_;
+    }
   };
+
 public:
   message_loop();
 
   void start();
   void stop();
 
-  void queue_task(task task_);
+  void queue_task(task task_,
+                  std::chrono::milliseconds delay = 0);
 
 private:
   void exec();
 
 //  std::atomic<bool> active_;
   bool active_;
-  thread_safe_queue<task> queue_;
+  std::priority_queue<queued_task> queue_;
   std::condition_variable waiter_;
 };
 }
