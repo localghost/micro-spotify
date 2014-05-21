25e7de66b0d1529ffb575ddd9e0053e1658e6039
localghost <localghost@codeleet.com>
2014-05-21 11:22:03 +0200

    Replace detil::high_steady_clock with std::conditional

--- base/chrono.h
+++ base/chrono.h
@@ -2,23 +2,27 @@
 #define BASE_CHRONO_H
 
 #include <chrono>
+#include <type_traits>
 
 namespace base {
-namespace detail {
-template <bool> struct high_steady_clock;
+//namespace detail {
+//template <bool> struct high_steady_clock;
+//
+//template <> struct high_steady_clock<true>
+//{
+//  typedef std::chrono::high_resolution_clock type;
+//};
+//
+//template <> struct high_steady_clock<false>
+//{
+//  typedef std::chrono::steady_clock type;
+//};
+//}
 
-template <> struct high_steady_clock<true>
-{
-  typedef std::chrono::high_resolution_clock type;
-};
-
-template <> struct high_steady_clock<false>
-{
-  typedef std::chrono::steady_clock type;
-};
-}
-
-typedef detail::high_steady_clock<std::chrono::high_resolution_clock::is_steady>::type high_steady_clock;
+typedef std::conditional<std::chrono::high_resolution_clock::is_steady,
+                         std::chrono::high_resolution_clock,
+                         std::chrono::steady_clock>::type
+                         high_steady_clock;
 }
 
 #endif
