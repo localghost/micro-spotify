a71efa659e08f5e4db19a0856b47115aec0dc2de
localghost <localghost@codeleet.com>
2014-05-29 18:21:57 +0200

    A few fixes in GTM

--- engine/global_threads_manager.cc
+++ engine/global_threads_manager.cc
@@ -12,9 +12,10 @@ base::thread& global_threads_manager::get_thread(global_thread_id id)
   return gtm.threads_[thread_index(id)];
 }
 
-inline std::size_t global_threads_manager::thread_index(global_thread_id id) const
+inline std::size_t global_threads_manager::thread_index(global_thread_id id)
 {
   return static_cast<std::size_t>(id);
+}
 
 global_threads_manager::global_threads_manager()
 {
@@ -30,6 +31,5 @@ global_threads_manager::~global_threads_manager()
 {
   for (std::size_t i = thread_index(global_thread_id::thread_count) - 1; i >= 0; --i)
     threads_[i].stop();
-}
 }
 }
--- engine/global_threads_manager.h
+++ engine/global_threads_manager.h
@@ -15,15 +15,15 @@ enum struct global_thread_id : std::size_t
 class global_threads_manager FINAL
 {
 public:
-  static base::thread& get_thread();
+  static base::thread& get_thread(global_thread_id id);
 
 private:
-  static std::size_t thread_index(global_thread_id id) const;
+  static std::size_t thread_index(global_thread_id id);
 
   global_threads_manager();
   ~global_threads_manager();
 
-  base::thread threads_[global_thread_id::thread_count];
+  base::thread threads_[static_cast<std::size_t>(global_thread_id::thread_count)];
 };
 
 inline base::thread& spotify_thread()
