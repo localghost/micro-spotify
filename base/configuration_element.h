55ca7072e66c5a5d0e5a9434e6b728e4a7249398
localghost <localghost@codeleet.com>
2014-05-29 17:34:18 +0200

    Make configuration_element thread-safe.
    Lock-based solution.
    
    Fixes #16

--- base/configuration_element.h
+++ base/configuration_element.h
@@ -2,6 +2,7 @@
 #define BASE_CONFIGURATION_ELEMENT_H
 
 #include <utility>
+#include <mutex>
 #include <boost/signals2.hpp>
 
 #include <base/compatibility.h>
@@ -10,31 +11,41 @@ namespace base {
 template<typename T>
 class configuration_element FINAL
 {
-    typedef boost::signals2::signal<void(T)> signal_type;
+  typedef boost::signals2::signal<void(T)> signal_type;
 
 public:
-    typedef T value_type;
-    typedef typename signal_type::slot_type slot_type;
+  typedef T value_type;
+  typedef typename signal_type::slot_type slot_type;
 
-    configuration_element() = default;
+  configuration_element() = default;
 
-    configuration_element(T value) : value_(std::move(value)) { }
+  configuration_element(T value) : value_(std::move(value)) { }
 
-    T value() const { return value_; }
-    void set_value(T value)
+  T value() const
+  {
+    std::lock_guard<std::mutex> lock(mutex_);
+
+    return value_;
+  }
+
+  void set_value(T value)
+  {
     {
-        value_ = value;
-        on_change_(std::move(value));
+      std::lock_guard<std::mutex> lock(mutex_);
+      value_ = value;
     }
+    on_change_(std::move(value));
+  }
 
-    boost::signals2::connection connect(const slot_type& slot)
-    {
-        return on_change_.connect(slot);
-    }
+  boost::signals2::connection connect(const slot_type& slot)
+  {
+    return on_change_.connect(slot);
+  }
 
 private:
-    T value_;
-    signal_type on_change_;
+  T value_;
+  mutable std::mutex mutex_;
+  signal_type on_change_;
 };
 }
 
