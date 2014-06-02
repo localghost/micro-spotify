20cf985224d7e14069d8003a1f4fe25777c07896
localghost <localghost@codeleet.com>
2014-06-02 19:20:59 +0200

    A few small fixes in log
    Fixes #9

--- base/log.h
+++ base/log.h
@@ -40,23 +40,14 @@ extern ostream_voidifier voidifier;
 #define LOG(Severity) \
     ::base::log::message{Severity, __FILE__, __LINE__, __FUNCTION__}.stream()
 
-// If Condition is false then the branch with the Stream
-// will not be evaluated.
-// Relies on the std::ostream operator bool() but its result
-// is not important in this case. It is only used as it is
-// the simplest way to convert the expression with Stream
-// to bool.
-// TODO Verify that neither GCC nor Clang prints warning
-//      like "value computed is not used" or similar
 #define LOG_IF(Condition, Severity) \
-    (Condition) ? static_cast<void>(0) : ::base::log::voidifier & LOG(Severity)
+    !(Condition) ? static_cast<void>(0) : ::base::log::voidifier & LOG(Severity)
 
 #define LOG_INFO LOG(::base::log::severity::Info)
         
 #ifdef DEBUG_BUILD
 #define LOG_DEBUG LOG_IF(true, ::base::log::severity::Debug)
 #else
-// FIXME prints warning in GCC 4.6
 #define LOG_DEBUG LOG_IF(false, ::base::log::severity::Debug)
 #endif
 
