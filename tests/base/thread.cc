bfd0dfecabaf4451dd1f67c7c582714e493e026f
localghost <localghost@codeleet.com>
2014-06-09 15:39:31 +0200

    Add simple test for base::thread
    Fixes #19

---
+++ tests/base/thread.cc
@@ -0,0 +1,25 @@
+#include <boost/test/unit_test.hpp>
+
+#include <atomic>
+#include <chrono>
+#include <boost/scope_exit.hpp>
+#include <base/task.h>
+#include <base/thread.h>
+#include <base/log.h>
+
+BOOST_AUTO_TEST_SUITE(TSBaseThread)
+
+BOOST_AUTO_TEST_CASE(TCBaseThread)
+{
+  std::atomic<int> counter{0};
+  base::thread th;
+  {
+    th.start();
+    BOOST_SCOPE_EXIT_ALL(&th) { th.stop(); };
+    th.queue_task(base::task{[&counter]{ ++counter; }});
+    th.queue_task(base::task{[&counter]{ ++counter; }});
+  }
+  BOOST_CHECK_EQUAL(counter, 2);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
