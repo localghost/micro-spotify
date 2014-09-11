090fad441b242756181e7b2370de8cadba04c0f4
localghost <localghost@codeleet.com>
2014-09-12 00:12:03 +0200

    Code style change

--- base/log.cc
+++ base/log.cc
@@ -8,45 +8,45 @@ namespace log {
 namespace {
 const char* severity_to_string(severity severity_)
 {
-    switch (severity_)
-    {
+  switch (severity_)
+  {
     case severity::Info: return "INFO";
     case severity::Debug: return "DEBUG";
     case severity::Warning: return "WARNING";
     case severity::Error: return "ERROR";
-    }
-    return ""; // to surpress warnings
+  }
+  return ""; // to surpress warnings
 }
 }
 
 ostream_voidifier voidifier{};
 
 message::message(severity severity_,
-                 const char* file,
-                 unsigned int line,
-                 const char* function)
+    const char* file,
+    unsigned int line,
+    const char* function)
 {
-    oss << "["
-        << severity_to_string(severity_) << ":"
-        // FIXME cut down the file to relative path
-        << file << ":"
-        << line << ":"
-        << function
-        << "] ";
+  oss << "["
+    << severity_to_string(severity_) << ":"
+    // FIXME cut down the file to relative path
+    << file << ":"
+    << line << ":"
+    << function
+    << "] ";
 }
 
 std::ostream& message::stream()
 {
-    return oss;
+  return oss;
 }
 
 message::~message()
 {
   static std::mutex m;
   std::lock_guard<std::mutex> guard{m};
-    // TODO check if operator<<() does not throw
-    // FIXME lock for thread-safety
-    std::cout << oss.str() << std::endl;
+  // TODO check if operator<<() does not throw
+  // FIXME lock for thread-safety
+  std::cout << oss.str() << std::endl;
 }
 }
 }
