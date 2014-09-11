41e8e964de7899f98144de95186e2bc9b51c879b
localghost <zkostrzewa@gmail.com>
2014-09-11 10:47:48 +0200

    Init engine session tests

---
+++ tests/engine/main.cc
@@ -0,0 +1,6 @@
+#ifndef BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MAIN
+#endif // BOOST_TEST_DYN_LINK
+
+#define BOOST_TEST_MODULE Engine
+#include <boost/test/unit_test.hpp>
---
+++ tests/engine/session.cc
@@ -0,0 +1,28 @@
+#include <boost/test/unit_test.hpp>
+
+#include <base/exception.h>
+#include <engine/configuration.h>
+#include <engine/session.h>
+
+BOOST_AUTO_TEST_SUITE(TSEngineSession)
+
+BOOST_AUTO_TEST_CASE(TCEngineSession)
+{
+  bool success = false;
+  std::string message;
+
+  try 
+  {
+    engine::configuration config;
+    engine::session s{config};
+  }
+  catch (base::exception)
+  {
+    succcess = false;
+    message = boost::current_exception_diagnostic_information();
+  }
+
+  BOOST_CHECK_MESSAGE(success, message);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
