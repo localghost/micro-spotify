2bcfa6f2b598db916643332dacc9b40c3f28e8f7
localghost <localghost@codeleet.com>
2014-09-14 22:25:04 +0200

    Init playlist_container

--- engine/playlist_container.h
+++ engine/playlist_container.h
@@ -5,18 +5,8 @@ namespace engine {
 class playlist_container
 {
 public:
-  playlist create();
-  playlist get(size_t n);
-  void remove(size_t n);
-  void move(size_t n, size_t position);
-
-  size_t size(); // sp_playlistcontainer_num_playlists()
-
-
-private:
-  playlist_container(sp_playlist_container* container) : container{container} {}
-
-  sp_playlistcontainer* container;
+  void add_playlist();
+  void create_playlist();
 };
 }
 
