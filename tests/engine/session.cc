7bd2a81d206e3ae3afbdcf00ea6feef06bd4f530
localghost <localghost@codeleet.com>
2014-09-14 22:30:23 +0200

    Add log out test for session
    All other tests are disabled due to a limit in spotify of one session
    per process.

--- tests/engine/session.cc
+++ tests/engine/session.cc
@@ -10,29 +10,65 @@
 
 BOOST_AUTO_TEST_SUITE(TSEngineSession)
 
-// Tests whether engine::session object can be created.
-BOOST_AUTO_TEST_CASE(TCEngineSessionCreate)
-{
-  bool success = true;
-  std::string message;
+//// Tests whether engine::session object can be created.
+//BOOST_AUTO_TEST_CASE(TCEngineSessionCreate)
+//{
+//  bool success = true;
+//  std::string message;
+//
+//  try 
+//  {
+//    engine::configuration config;
+//    engine::session s{config};
+//  }
+//  catch (base::exception)
+//  {
+//    success = false;
+//    message = boost::current_exception_diagnostic_information();
+//  }
+//
+//  BOOST_CHECK_MESSAGE(success, message);
+//}
 
-  try 
-  {
-    engine::configuration config;
-    engine::session s{config};
-  }
-  catch (base::exception)
-  {
-    success = false;
-    message = boost::current_exception_diagnostic_information();
-  }
+//// Checks whether it is possible to log in into spotify via
+//// engine::session API.
+//BOOST_AUTO_TEST_CASE(TCEngineSessionLogIn)
+//{
+//  bool success = false;
+//  std::string message;
+//  std::mutex m;
+//  std::condition_variable cv;
+//
+//  try 
+//  {
+//    engine::configuration config;
+//    engine::session s{config};
+//    s.connect_logged_in([&](sp_error err)
+//        {
+//          {
+//            std::lock_guard<std::mutex> guard{m};
+//            if (SP_ERROR_OK == err)
+//              success = true;
+//          }
+//          cv.notify_one();
+//        });
+//    s.log_in();
+//    std::unique_lock<std::mutex> guard{m};
+//    if (!cv.wait_for(guard, std::chrono::seconds{5}, [&]{ return success; }))
+//      message = "log in slot not called";
+//  }
+//  catch (base::exception)
+//  {
+//    success = false;
+//    message = boost::current_exception_diagnostic_information();
+//  }
+//
+//  BOOST_CHECK_MESSAGE(success, message);
+//}
 
-  BOOST_CHECK_MESSAGE(success, message);
-}
-
-// Checks whether it is possible to log in into spotify via
+// Checks whether it is possible to log in/log out into spotify via
 // engine::session API.
-BOOST_AUTO_TEST_CASE(TCEngineSessionLogIn)
+BOOST_AUTO_TEST_CASE(TCEngineSessionLogOut)
 {
   bool success = false;
   std::string message;
@@ -53,9 +89,32 @@ BOOST_AUTO_TEST_CASE(TCEngineSessionLogIn)
           cv.notify_one();
         });
     s.log_in();
-    std::unique_lock<std::mutex> guard{m};
-    if (!cv.wait_for(guard, std::chrono::seconds{5}, [&]{ return success; }))
-      message = "log in slot not called";
+    {
+      std::unique_lock<std::mutex> guard{m};
+      if (!cv.wait_for(guard, std::chrono::seconds{5}, [&]{ return success; }))
+        message = "log in slot not called";
+    }
+
+    BOOST_CHECK_MESSAGE(success, message);
+
+    if (!success) return;
+
+    success = false;
+
+    s.connect_logged_out([&]
+        {
+          {
+            std::lock_guard<std::mutex> guard{m};
+            success = true;
+          }
+          cv.notify_one();
+        });
+    s.log_out();
+    {
+      std::unique_lock<std::mutex> guard{m};
+      if (!cv.wait_for(guard, std::chrono::seconds{5}, [&]{ return success; }))
+        message = "log out slot not called";
+    }
   }
   catch (base::exception)
   {
