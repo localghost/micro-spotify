4bcc97aafaa129333c31afc5d9e0a6cedbfed833
localghost <localghost@codeleet.com>
2014-09-21 22:39:18 +0200

    Make frame copyable - but ugly

--- engine/frame.h
+++ engine/frame.h
@@ -1,15 +1,17 @@
 #ifndef ENGINE_FRAME_H
 #define ENGINE_FRAME_H
 
+#include <memory>
+
 namespace engine {
 // FIXME logically a frame should be a copyable object
 //       this is a quick fix
-struct frame : boost::noncopyable
+struct frame
 {
   unsigned rate;
   unsigned channels;
   unsigned samples;
-  const void* data;
+  std::shared_ptr<const int16_t> data;
 };
 }
 #endif
