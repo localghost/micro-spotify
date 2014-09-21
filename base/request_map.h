28e6ab54edae2a7fdf870a0e9f3efa27a2c383e2
localghost <localghost@codeleet.com>
2014-09-21 22:38:45 +0200

    Add init version of request_map

---
+++ base/request_map.h
@@ -0,0 +1,41 @@
+#ifndef BASE_REQUEST_MAP_H
+#define BASE_REQUEST_MAP_H
+
+#include <map>
+
+namespace base {
+template<typename T>
+class request_map
+{
+public:
+  typedef unsigned long id_type;
+  typedef T request_type;
+
+  template<typename U>
+  id_type insert(U&& request)
+  {
+    static_assert(std::is_same<request_type, U>::value, "Class's and method's template arguments differ");
+
+    id_type id = next_id++;
+    // FIXME detect collisions and re-use free ids
+    requests[id] = std::forward<U>(request);
+    return id;
+  }
+
+  void erase(id_type id)
+  {
+    requests.erase(id);
+  }
+
+  T& get(id_type id)
+  {
+    return requests[id];
+  }
+
+private:
+  id_type next_id = 0;
+  std::map<id_type, request_type> requests;
+};
+}
+
+#endif
