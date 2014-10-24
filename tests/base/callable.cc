b745563d408050f2ac2f64f68db02df97827caeb
localghost <zkostrzewa@gmail.com>
2014-10-24 08:13:58 +0200

    Add a few tests for base::callable

--- tests/base/callable.cc
+++ tests/base/callable.cc
@@ -1,15 +1,74 @@
 #include <boost/test/unit_test.hpp>
 
+#include <functional>
 #include <base/callable.h>
 
 BOOST_AUTO_TEST_SUITE(TSBaseCallable)
 
-BOOST_AUTO_TEST_CASE(TCBaseCallableNoArgs)
+/// Uses callable with lambda
+BOOST_AUTO_TEST_CASE(TCCallable1)
 {
-    bool pass = false;
-    base::callable call{[&pass]() { pass = true; }};
-    call();
-    BOOST_CHECK(pass);
+  unsigned counter = 0;
+
+  base::callable{[&]{++counter;}}();
+
+  BOOST_CHECK_EQUAL(counter, 1);
+}
+
+/// Uses callable with a functor
+BOOST_AUTO_TEST_CASE(TCCallable2)
+{
+  struct callee
+  {
+    explicit callee(unsigned& counter) : counter{counter} {}
+    void operator()()
+    {
+      ++counter;
+    }
+    unsigned& counter;
+  };
+  unsigned counter = 0;
+
+  base::callable{callee{counter}}();
+
+  BOOST_CHECK_EQUAL(counter, 1);
+}
+
+/// Uses callable with a method (via std::bind)
+BOOST_AUTO_TEST_CASE(TCCallable3)
+{
+  struct callee
+  {
+    explicit callee(unsigned& counter) : counter{counter} {}
+    void call()
+    {
+      ++counter;
+    }
+    unsigned& counter;
+  };
+
+  unsigned counter = 0;
+  callee action{counter};
+
+  base::callable{std::bind(&callee::call, &action)}();
+
+  BOOST_CHECK_EQUAL(counter, 1);
+}
+
+namespace {
+void TCCallable4_callee(unsigned& counter)
+{
+  ++counter;
+}
+}
+/// Uses callable with a function (via std::bind)
+BOOST_AUTO_TEST_CASE(TCCallable4)
+{
+  unsigned counter = 0;
+
+  base::callable{std::bind(&TCCallable4_callee, std::ref(counter))}();
+
+  BOOST_CHECK_EQUAL(counter, 1);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
